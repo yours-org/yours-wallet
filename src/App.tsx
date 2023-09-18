adccf1712b9759fae272c84d33c86bfde03ddd47
Dan Wagner <dan.wagner06@gmail.com>
2023-09-17 23:40:46 -0700

    update routes and add bottom menu provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
-import { Start } from "./pages/Start";
+import { Start } from "./pages/onboarding/Start";
 import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import { colors } from "./colors";
-import { CreateWallet } from "./pages/CreateWallet";
 import { SnackbarProvider } from "./contexts/SnackbarContext";
+import { CreateWallet } from "./pages/onboarding/CreateWallet";
+import { Bsv } from "./pages/Bsv";
+import { Nfts } from "./pages/Nfts";
+import { BottomMenuProvider } from "./contexts/BottomMenuContext";
+import { Settings } from "./pages/Settings";
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 21.875rem;
-  height: 31.25rem;
+  width: 22.5rem;
+  height: 33.75rem;
   background-color: ${colors.navy};
   position: relative;
 `;
@@ -18,13 +22,17 @@ export const App = () => {
   return (
     <Container>
       <SnackbarProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/create-wallet" element={<CreateWallet />} />
-            <Route path="/wallet" element={<div>My Wallet</div>} />
-          </Routes>
-        </Router>
+        <BottomMenuProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Start />} />
+              <Route path="/create-wallet" element={<CreateWallet />} />
+              <Route path="/bsv" element={<Bsv />} />
+              <Route path="/nfts" element={<Nfts />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </Router>
+        </BottomMenuProvider>
       </SnackbarProvider>
     </Container>
   );
