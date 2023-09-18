87b65c999a8ab3b46c093aa0b5baa2a384060519
Dan Wagner <dan.wagner06@gmail.com>
2023-09-17 23:45:52 -0700

    create QR code component

---
+++ src/components/QrCode.tsx
@@ -0,0 +1,36 @@
+import * as qr from "qrcode";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+const QrImage = styled.img`
+  width: 10rem;
+  height: 10rem;
+  cursor: pointer;
+`;
+
+export type QrCodeProps = {
+  address: string;
+  onClick: () => void;
+};
+
+export const QrCode = (props: QrCodeProps) => {
+  const { address, onClick } = props;
+  const [qrUrl, setQrUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const uri = `bitcoin:${address}`;
+    qr.toDataURL(uri, function (err, url) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      setQrUrl(url);
+    });
+  }, [address]);
+
+  return (
+    <>
+      {qrUrl && <QrImage src={qrUrl} alt="Bitcoin QR Code" onClick={onClick} />}
+    </>
+  );
+};
