446499181b8447aad82d5aa281ff9b5389bbd45a
David Case <david@fyxgaming.com>
2023-10-12 10:15:02 -0400

    v3-api initial commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const WOC_BASE_URL = "https://api.whatsonchain.com/v1/bsv/main";
-export const GP_BASE_URL = "https://ordinals.gorillapool.io/api";
+export const GP_BASE_URL = "https://v3.ordinals.gorillapool.io/api";
 export const GORILLA_POOL_ARC_URL = "https://arc.gorillapool.io/v1";
 export const BSV_DECIMAL_CONVERSION = 100000000;
 export const FEE_PER_BYTE = 0.02;
