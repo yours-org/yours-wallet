3a80d1a7e7e77a0cc41d9331c0c9ac96b9d7069e
Dan Wagner <dan.wagner06@gmail.com>
2023-10-27 12:37:48 -0700

    refactor/move funcs to useGorillaPool

--- src/hooks/useGorillaPool.ts
+++ src/hooks/useGorillaPool.ts
@@ -3,6 +3,7 @@ import { GP_BASE_URL, GP_TESTNET_BASE_URL } from "../utils/constants";
 import { NetWork } from "../utils/network";
 import { useNetwork } from "./useNetwork";
 import { OrdinalResponse, OrdinalTxo } from "./useOrds";
+import { Script } from "bsv-wasm-web";
 
 type GorillaPoolErrorMessage = {
   message: string;
@@ -54,8 +55,41 @@ export const useGorillaPool = () => {
     }
   };
 
+  const getUtxoByOutpoint = async (outpoint: string): Promise<OrdinalTxo> => {
+    try {
+      const { data } = await axios.get(
+        `${getOrdinalsBaseUrl()}/api/txos/${outpoint}?script=true`
+      );
+      const ordUtxo = data;
+
+      ordUtxo.script = Script.from_bytes(
+        Buffer.from(ordUtxo.script, "base64")
+      ).to_asm_string();
+
+      return ordUtxo;
+    } catch (e) {
+      throw new Error(JSON.stringify(e));
+    }
+  };
+
+  const getMarketData = async (outpoint: string) => {
+    try {
+      const res = await axios.get(
+        `${getOrdinalsBaseUrl()}/api/inscriptions/${outpoint}?script=true`
+      );
+      const data = res.data as OrdinalTxo;
+      if (!data?.script || !data.origin?.outpoint.toString())
+        throw new Error("Could not get listing script");
+      return { script: data.script, origin: data.origin.outpoint.toString() };
+    } catch (error) {
+      throw new Error(`Error getting market data: ${JSON.stringify(error)}`);
+    }
+  };
+
   return {
     getOrdUtxos,
     broadcastWithGorillaPool,
+    getUtxoByOutpoint,
+    getMarketData,
   };
 };
