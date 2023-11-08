8ca18003ae07178869f003570844269ffb476324
hh <hf.zheng@scrypt.io>
2023-11-08 18:25:19 +0800

    GetSignaturesRequest auto close

--- src/pages/requests/GetSignaturesRequest.tsx
+++ src/pages/requests/GetSignaturesRequest.tsx
@@ -201,7 +201,6 @@ export const GetSignaturesRequest = (props: GetSignaturesRequestProps) => {
   const { setSelected } = useBottomMenu();
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const { addSnackbar, message } = useSnackbar();
-  const navigate = useNavigate();
   const { isPasswordRequired } = useWeb3Context();
 
   const { getSigsRequest, onSignature, popupId } = props;
@@ -232,7 +231,7 @@ export const GetSignaturesRequest = (props: GetSignaturesRequestProps) => {
     await sleep(25);
 
     if (!passwordConfirm && isPasswordRequired) {
-      addSnackbar('You must enter a password!', 'error');
+      addSnackbar('You must enter a password!', 'error', 3000);
       setIsProcessing(false);
       return;
     }
@@ -247,34 +246,35 @@ export const GetSignaturesRequest = (props: GetSignaturesRequestProps) => {
           ? 'Unknown Address: ' + (getSigsRes.error.cause ?? '')
           : 'An unknown error has occurred! Try again.';
 
-      addSnackbar(message, 'error', 5000);
+      chrome.runtime.sendMessage({
+        action: 'getSignaturesResponse',
+        ...getSigsRes,
+      });
 
-      if (getSigsRes.error.message === 'invalid-password') {
-        // could try again only if the password is wrong
-        setIsProcessing(false);
-        return;
-      }
+      setIsProcessing(false);
+
+      addSnackbar(message, 'error', 3000);
+
+      return
     }
 
-    if (getSigsRes?.sigResponses) {
-      addSnackbar('Successfully Signed!', 'success');
-    }
 
     setGetSigsResponse(getSigsRes.sigResponses);
-
-    setTimeout(() => {
-      onSignature();
-      if (!getSigsRes && popupId) chrome.windows.remove(popupId);
-      storage.remove('getSignaturesRequest');
-      navigate('/bsv-wallet');
-    }, 2000);
-
     chrome.runtime.sendMessage({
       action: 'getSignaturesResponse',
       ...getSigsRes,
     });
 
     setIsProcessing(false);
+    
+    addSnackbar('Successfully Signed!', 'success');
+
+    setTimeout(() => {
+      onSignature();
+      storage.remove('getSignaturesRequest');
+      if (popupId) chrome.windows.remove(popupId);
+    }, 2000);
+
   };
 
   const rejectSigning = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
