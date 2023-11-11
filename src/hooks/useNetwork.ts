ae0d1a32ac2ecf1a4dcea6d01a1dc70e0f58199a
Dan Wagner <dan.wagner06@gmail.com>
2023-11-11 10:24:18 -0800

    fix: address network requests on wrong network

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { storage } from '../utils/storage';
 import { NetWork, NetWorkStorage } from '../utils/network';
+import { storage } from '../utils/storage';
 
 export const useNetwork = () => {
   const [network, setNetwork] = useState(NetWork.Mainnet);
@@ -29,8 +29,18 @@ export const useNetwork = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const isAddressOnRightNetwork = (address: string) => {
+    switch (network) {
+      case NetWork.Mainnet:
+        return address.startsWith('1');
+      case NetWork.Testnet:
+        return !address.startsWith('1');
+    }
+  };
+
   return {
     network,
     setNetwork,
+    isAddressOnRightNetwork,
   };
 };
