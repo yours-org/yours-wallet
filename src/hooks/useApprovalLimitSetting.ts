a75877b718d4380b6d488739edb221590b2847be
Dan Wagner <dan.wagner06@gmail.com>
2024-06-26 06:25:33 -0700

    fix: typo

--- src/hooks/useApprovalLimitSetting.ts
+++ src/hooks/useApprovalLimitSetting.ts
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react';
 import { storage } from '../utils/storage';
 
 export const useNoApprovalLimitSetting = () => {
-  const [noApprovalLimit, setNoAprrovalLimit] = useState<number | undefined>();
+  const [noApprovalLimit, setNoApprovalLimit] = useState<number | undefined>();
 
   const retrieveLimit = (): Promise<boolean> => {
     return new Promise((resolve, reject) => {
       storage.get(['noApprovalLimit'], async (res) => {
         try {
           const limit = res.noApprovalLimit ?? 0;
-          setNoAprrovalLimit(limit);
+          setNoApprovalLimit(limit);
           resolve(res.noApprovalLimit);
         } catch (error) {
           reject(error);
@@ -25,6 +25,6 @@ export const useNoApprovalLimitSetting = () => {
 
   return {
     noApprovalLimit,
-    setNoAprrovalLimit,
+    setNoApprovalLimit,
   };
 };
