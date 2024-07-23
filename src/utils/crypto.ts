0d40a99175565b4ddeee0c4ae26f567c12c483b2
David Case <david@fyxgaming.com>
2024-07-23 16:39:40 -0400

    implement encrypt/decrypt

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,4 +1,4 @@
-import { PrivateKey, PublicKey } from '@bsv/sdk';
+import { ECIES, PrivateKey, PublicKey, Utils } from '@bsv/sdk';
 import CryptoJS from 'crypto-js';
 
 export const deriveKey = (password: string, salt: string) => {
@@ -56,13 +56,16 @@ export const encryptUsingPrivKey = (
   pubKeys: PublicKey[],
   privateKey: PrivateKey,
 ) => {
-  // const msgBuf = Buffer.from(message, encoding);
+  const msgBuf = Utils.toArray(message, encoding);
+  return pubKeys.map((pubKey) => Utils.toBase64(ECIES.electrumEncrypt(msgBuf, pubKey, privateKey)));
+
   // const encryptedMessages = pubKeys.map((keys) => keys.encrypt_message(msgBuf, privateKey));
   // return encryptedMessages.map((m) => Buffer.from(m.to_bytes()).toString('base64'));
-  return [''];
+  // return [''];
 };
 
 export const decryptUsingPrivKey = (messages: string[], privateKey: PrivateKey) => {
+  return messages.map((m) => Utils.toBase64(ECIES.electrumDecrypt(Utils.toArray(m, 'base64'), privateKey)));
   // const decryptedMessages: string[] = [];
   // for (const message of messages) {
   //   const ciphertext = ECIESCiphertext.from_bytes(Buffer.from(message, 'base64'), true);
@@ -71,5 +74,5 @@ export const decryptUsingPrivKey = (messages: string[], privateKey: PrivateKey) 
   //   decryptedMessages.push(Buffer.from(decrypted).toString('base64'));
   // }
   // return decryptedMessages;
-  return [''];
+  // return [''];
 };
