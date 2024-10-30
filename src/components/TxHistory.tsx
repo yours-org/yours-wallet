a2bf3b47612f6c62695284b7b1883d4a3e0caa12
Dan Wagner <dan.wagner06@gmail.com>
2024-10-29 17:12:54 -0700

    refactor: WIP polish

--- src/components/TxHistory.tsx
+++ src/components/TxHistory.tsx
@@ -4,14 +4,12 @@ import { useServiceContext } from '../hooks/useServiceContext';
 import { useEffect, useMemo, useState } from 'react';
 import { HeaderText, Text } from './Reusable';
 import { GENERIC_TOKEN_ICON, URL_WHATSINCHAIN } from '../utils/constants';
-import { FaTimes, FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import { FaTimes, FaChevronDown, FaChevronUp, FaLink, FaTag } from 'react-icons/fa'; // Import FaTag
 import { TxLog } from 'spv-store';
-import transactions from './const';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 import { Button } from './Button';
 import bsvCoin from '../assets/bsv-coin.svg';
 import lock from '../assets/lock.svg';
-import priceTag from '../assets/price-tag.svg';
+import { Show } from './Show';
 
 const slideIn = keyframes`
   from {
@@ -48,9 +46,16 @@ const HistoryRow = styled.div<WhiteLabelTheme>`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: ${({ theme }) => theme.color.global.row};
   width: 95%;
-  padding: 0.35rem;
-  border-bottom: 1px solid ${({ theme }) => theme.color.global.gray + '50'};
+  padding: 0.5rem 1rem;
+  border: 1px solid ${({ theme }) => theme.color.global.gray + '50'};
+  border-radius: 0.5rem;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
 const Icon = styled.img`
@@ -62,14 +67,15 @@ const Icon = styled.img`
 const TickerWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: between;
+  justify-content: space-between;
 `;
 
 const ButtonsWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: between;
+  justify-content: space-between;
   width: 40%;
+  margin: 1rem 0;
 `;
 
 const TickerTextWrapper = styled.div`
@@ -92,11 +98,11 @@ const BackWrapper = styled.div`
 `;
 
 const RowWrapper = styled.div`
-  width: 100%;
+  width: 95%;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0.5rem 0;
+  margin: 0.15rem 0;
 `;
 
 const BoundedContent = styled.div`
@@ -118,6 +124,18 @@ const IconContent = styled.div`
   width: 2.5rem;
 `;
 
+const ListIconWrapper = styled.div<WhiteLabelTheme>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.color.global.contrast};
+  width: 2.25rem;
+  height: 2.25rem;
+`;
+
+type Tag = 'bsv21' | 'origin' | 'list' | 'lock' | 'fund';
+
 export type TxHistoryProps = {
   theme: Theme;
   onBack: () => void;
@@ -125,19 +143,22 @@ export type TxHistoryProps = {
 
 export const TxHistory = (props: TxHistoryProps) => {
   const { theme, onBack } = props;
-  const [data, setData] = useState<TxLog[]>(); // ! api response
+  const [data, setData] = useState<TxLog[]>();
   const [isSlidingOut, setIsSlidingOut] = useState(false);
   const { oneSatSPV } = useServiceContext();
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 15; // ! you can change the number of items per page
-  const dataTest = transactions; // ! delete
+  const itemsPerPage = 25;
   const { gorillaPoolService, chromeStorageService } = useServiceContext();
   const [expandedRows, setExpandedRows] = useState<Set<number>>(new Set());
 
+  const tagPriorityOrder: Tag[] = ['list', 'bsv21', 'origin', 'lock', 'fund']; // The order of these tags will determine the order of the icons and which is prioritized
+
   useEffect(() => {
     const fetchData = async () => {
+      if (!oneSatSPV) return;
       try {
         const tsx = await oneSatSPV.getRecentTxs();
+        console.log(tsx);
         setData(tsx);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -145,7 +166,7 @@ export const TxHistory = (props: TxHistoryProps) => {
     };
 
     fetchData();
-  }, []); // ! add dependency of the fetch data
+  }, [oneSatSPV]);
 
   const handleBackClick = () => {
     setIsSlidingOut(true);
@@ -155,12 +176,11 @@ export const TxHistory = (props: TxHistoryProps) => {
   const paginatedData = useMemo(() => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    return dataTest?.slice(startIndex, endIndex); // ! change dataTest to data
-  }, [currentPage, dataTest]); // ! change dataTest to data
+    return data?.slice(startIndex, endIndex);
+  }, [currentPage, data]);
 
   const handleNextPage = () => {
-    if (currentPage * itemsPerPage < (dataTest?.length ?? 0)) {
-      // ! change dataTest to data
+    if (currentPage * itemsPerPage < (data?.length ?? 0)) {
       setCurrentPage(currentPage + 1);
     }
   };
@@ -175,19 +195,36 @@ export const TxHistory = (props: TxHistoryProps) => {
     window.open(`${URL_WHATSINCHAIN}${txid}`, '_blank');
   };
 
-  const getIconForSummary = (tag: string, icon: string) => {
+  const getIconForSummary = (tag: Tag, icon?: string) => {
     switch (tag) {
       case 'fund':
-        return bsvCoin;
+        return <Icon src={bsvCoin} alt="Fund Icon" />;
       case 'lock':
-        return lock;
+        return <Icon src={lock} alt="Lock Icon" />;
       case 'list':
-        return priceTag;
+        return (
+          <ListIconWrapper theme={theme}>
+            <FaTag style={{ width: '1rem', height: '1rem', color: theme.color.global.neutral }} />
+          </ListIconWrapper>
+        );
       default:
-        return icon
-          ? `${gorillaPoolService.getBaseUrl(chromeStorageService.getNetwork())}/content/${icon}`
-          : GENERIC_TOKEN_ICON;
+        return icon ? (
+          <Icon
+            src={`${gorillaPoolService.getBaseUrl(chromeStorageService.getNetwork())}/content/${icon}`}
+            alt="Summary Icon"
+          />
+        ) : (
+          <Icon src={GENERIC_TOKEN_ICON} alt="Generic Token Icon" />
+        );
     }
+  };
+
+  const sortEntriesByPriority = (entries: [Tag, { icon?: string; amount?: number }][]) => {
+    return entries.sort((a, b) => {
+      const aPriority = tagPriorityOrder.indexOf(a[0]);
+      const bPriority = tagPriorityOrder.indexOf(b[0]);
+      return (aPriority === -1 ? Infinity : aPriority) - (bPriority === -1 ? Infinity : bPriority);
+    });
   };
 
   const toggleRowExpansion = (idx: number) => {
@@ -209,58 +246,47 @@ export const TxHistory = (props: TxHistoryProps) => {
       </Text>
       {(paginatedData || []).length > 0 ? (
         paginatedData?.map((t) => {
-          const summaryEntries = Object.entries(t.summary);
+          const summaryEntries = sortEntriesByPriority(
+            Object.entries(t.summary || {}).filter(([key]) => tagPriorityOrder.includes(key as Tag)) as [
+              Tag,
+              { icon?: string; amount?: number },
+            ][],
+          );
           const isExpanded = expandedRows.has(t.idx);
-
           return (
-            <RowWrapper>
+            <RowWrapper key={t.idx}>
               <HistoryRow
                 theme={theme}
-                key={t.idx}
                 onClick={summaryEntries.length > 1 ? () => toggleRowExpansion(t.idx) : undefined}
-                style={
-                  summaryEntries.length > 1
-                    ? {
-                        display: 'flex',
-                        alignItems: 'center',
-                        cursor: 'pointer',
-                        color: theme.color.global.gray,
-                      }
-                    : {
-                        display: 'flex',
-                        alignItems: 'center',
-                        color: theme.color.global.gray,
-                      }
-                }
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  cursor: summaryEntries.length > 1 ? 'pointer' : 'default',
+                  color: theme.color.global.gray,
+                }}
               >
-                <TickerWrapper
-                  style={{
-                    width: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '0.5rem',
-                  }}
-                >
+                <TickerWrapper style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
                   {summaryEntries.slice(0, isExpanded ? summaryEntries.length : 1).map(([key, value], idx) => (
                     <BoundedContent key={idx}>
                       <IconNameWrapper>
                         <IconContent>
-                          {!isExpanded &&
-                            summaryEntries.slice(0, 3).map(([key, value], iconIdx) => (
-                              <Icon
-                                key={iconIdx}
-                                src={getIconForSummary(key, value.icon)}
-                                style={{
-                                  position: 'absolute',
-                                  left: `${iconIdx * 0.75}rem`,
-                                  zIndex: 3 - iconIdx,
-                                }}
-                              />
-                            ))}
-                          {isExpanded && <Icon src={getIconForSummary(key, value.icon)} />}
+                          {isExpanded
+                            ? getIconForSummary(key, value.icon)
+                            : summaryEntries.slice(0, 3).map(([key, value], iconIdx) => (
+                                <div
+                                  key={iconIdx}
+                                  style={{
+                                    position: 'absolute',
+                                    left: `${iconIdx * 0.75}rem`,
+                                    zIndex: 3 - iconIdx,
+                                  }}
+                                >
+                                  {getIconForSummary(key, value.icon)}
+                                </div>
+                              ))}
                         </IconContent>
                         <TickerTextWrapper>
-                          <HeaderText style={{ fontSize: '0.85rem', marginTop: 0, marginLeft: '0.2rem' }} theme={theme}>
+                          <HeaderText style={{ fontSize: '0.85rem', marginTop: 0 }} theme={theme}>
                             {key}
                           </HeaderText>
                           <Text
@@ -272,9 +298,9 @@ export const TxHistory = (props: TxHistoryProps) => {
                               textAlign: 'left',
                             }}
                           >
-                            {value.amount < 0
+                            {value?.amount && value.amount < 0
                               ? value.amount === -1
-                                ? 'Listar'
+                                ? 'Listing'
                                 : 'Sent'
                               : value.amount === 0
                                 ? 'Cancelled'
@@ -287,27 +313,26 @@ export const TxHistory = (props: TxHistoryProps) => {
                           style={{
                             fontSize: '0.85rem',
                             marginTop: 0,
-                            color:
-                              value.amount < 0
-                                ? value.amount === -1
-                                  ? '#E5BE01'
-                                  : 'red'
-                                : value.amount === 0
-                                  ? theme.color.global.gray
-                                  : 'green',
-                            width: '60px',
+                            color: value?.amount
+                              ? value.amount > 1
+                                ? '#52C41A'
+                                : value.amount < -1
+                                  ? '#FF4D4F'
+                                  : 'transparent'
+                              : 'transparent',
                             textAlign: 'right',
                           }}
                           theme={theme}
                         >
                           {value.amount}
                         </HeaderText>
-                        <FaExternalLinkAlt
-                          onClick={() => handleOpenLink(t.txid)}
-                          style={{ cursor: 'pointer', color: theme.color.global.gray }}
-                          title="See transaction in Whatsonchain"
-                        />
-
+                        <Show when={idx === 0}>
+                          <FaLink
+                            onClick={() => handleOpenLink(t.txid)}
+                            style={{ cursor: 'pointer', color: theme.color.component.primaryButtonLeftGradient }}
+                            title="See transaction in Whatsonchain"
+                          />
+                        </Show>
                         {idx === 0 && summaryEntries.length > 1 ? (
                           isExpanded ? (
                             <FaChevronUp />
@@ -315,7 +340,7 @@ export const TxHistory = (props: TxHistoryProps) => {
                             <FaChevronDown />
                           )
                         ) : (
-                          <span style={{ display: 'inline-block', width: '12px', height: '16px' }} /> // empty space to keep the icons order
+                          <span style={{ display: 'inline-block', width: '12px', height: '16px' }} />
                         )}
                       </ContentWrapper>
                     </BoundedContent>
@@ -326,7 +351,7 @@ export const TxHistory = (props: TxHistoryProps) => {
           );
         })
       ) : (
-        <Text>No transaction records found.</Text>
+        <Text theme={theme}>No transaction records found.</Text>
       )}
       <ButtonsWrapper>
         <Button
@@ -339,10 +364,10 @@ export const TxHistory = (props: TxHistoryProps) => {
         />
         <Button
           theme={theme}
-          type="primary"
+          type="secondary"
           label="Next"
           onClick={handleNextPage}
-          disabled={currentPage * itemsPerPage >= (dataTest?.length ?? 0)} // ! change dataTest to data
+          disabled={currentPage * itemsPerPage >= (data?.length ?? 0)}
         />
       </ButtonsWrapper>
     </Container>
