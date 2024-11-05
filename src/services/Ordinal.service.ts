2e31f3d7959a3b60ab47d293d8a7ddc276591512
Dan Wagner <dan.wagner06@gmail.com>
2024-11-05 07:38:06 -0800

    fix: pk issue

--- src/services/Ordinal.service.ts
+++ src/services/Ordinal.service.ts
@@ -12,6 +12,7 @@ import {
   sendOrdinals,
   Utxo,
   Destination,
+  NftUtxo,
 } from 'js-1sat-ord';
 import { Bsv20, Ordinal, PaginatedOrdinalsResponse, PurchaseOrdinal } from 'yours-wallet-provider';
 import { P2PKH, PrivateKey, SatoshisPerKilobyte, Script, Transaction, Utils } from '@bsv/sdk';
@@ -185,8 +186,7 @@ export class OrdinalService {
       const keys = await this.keysService.retrieveKeys(password);
       if (!keys.walletWif || !keys.ordWif) return { error: 'no-keys' };
 
-      const paymentPk = PrivateKey.fromWif(keys.walletWif);
-      const ordPk = PrivateKey.fromWif(keys.ordWif);
+      const pkMap = await this.keysService.retrievePrivateKeyMap(password);
 
       const fundResults = await this.oneSatSPV.search(new TxoLookup('fund'));
       const outpointObjects = outpoints.map((outpoint) => new Outpoint(outpoint));
@@ -195,12 +195,15 @@ export class OrdinalService {
       const { activeUtxos, destinations }: { activeUtxos: Utxo[]; destinations: Destination[] } = ordUtxos.reduce(
         (acc: { activeUtxos: Utxo[]; destinations: Destination[] }, txo, index) => {
           if (txo) {
+            const pk = pkMap.get(txo.owner || '');
+            if (!pk) return acc;
             // Only process active UTXOs
             acc.activeUtxos.push({
               txid: txo.outpoint.txid,
               vout: txo.outpoint.vout,
               satoshis: 1,
               script: Buffer.from(txo.script).toString('base64'),
+              pk,
             });
             acc.destinations.push({
               address: destinationAddresses[index],
@@ -211,15 +214,21 @@ export class OrdinalService {
         { activeUtxos: [], destinations: [] },
       );
 
-      const { tx } = await sendOrdinals({
-        ordPk,
-        paymentPk,
-        paymentUtxos: fundResults.txos.map((t) => ({
+      const paymentUtxos: Utxo[] = [];
+      fundResults.txos.forEach((t) => {
+        const pk = pkMap.get(t.owner || '');
+        if (!pk) return;
+        paymentUtxos.push({
           txid: t.outpoint.txid,
           vout: t.outpoint.vout,
           satoshis: Number(t.satoshis),
           script: Buffer.from(t.script).toString('base64'),
-        })),
+          pk,
+        });
+      });
+
+      const { tx } = await sendOrdinals({
+        paymentUtxos,
         ordinals: activeUtxos,
         destinations: destinations,
         changeAddress: keys.walletAddress,
